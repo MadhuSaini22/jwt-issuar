df494391d85eb3d5b95669a48262e8184a0cbe42
feniljariwala82 <feniljariwala82@gmail.com>
2021-12-07 17:05:17 +0530

    v0.0.3

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import JwtUsingSecret from './JwtUsingSecret'
 import JwtUsingKeyPair from './JwtUsingKeyPair'
 
 export default {
-  JwtUsingKeyPair: JwtUsingKeyPair,
-  JwtUsingSecret: JwtUsingSecret,
+  JwtUsingKeyPair,
+  JwtUsingSecret,
 }
